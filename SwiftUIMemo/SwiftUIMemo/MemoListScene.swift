328fb2f75ba3bf23c449dce089c0023b2711b454
Jayfunf <simh3077@gmail.com>
2021-09-12 19:49:51 +0900

    init

---
+++ SwiftUIMemo/SwiftUIMemo/MemoListScene.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMemo
+//
+//  Created by Minhyun Cho on 2021/09/12.
+//
+
+import SwiftUI
+
+struct MemoListScene: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MemoListScene_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoListScene()
+    }
+}
