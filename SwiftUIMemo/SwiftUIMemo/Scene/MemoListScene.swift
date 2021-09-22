8f1a60fbaf3b818eba2a00a8da881fcecc682877
Jayfunf <simh3077@gmail.com>
2021-09-22 20:01:39 +0900

    list -> compose

--- SwiftUIMemo/SwiftUIMemo/Scene/MemoListScene.swift
+++ SwiftUIMemo/SwiftUIMemo/Scene/MemoListScene.swift
@@ -12,13 +12,31 @@ struct MemoListScene: View {
     @EnvironmentObject var store: MemoStore
     @EnvironmentObject var formatter: DateFormatter
     
+    @State var showComposer: Bool = false //컴포즈신 제어
+    
     var body: some View {
         NavigationView {
             List(store.list) { memo in
                 MemoCell(memo: memo)
             }
             .navigationBarTitle("내 메모")
+            .navigationBarItems(trailing: ModalButton(show: $showComposer))// 값 복사 X 바인딩이 저장됨. 뷰 외부를 바꾸고 싶다면 바인딩으로 $으로 해야함.
+            .sheet(isPresented: $showComposer, content: {
+                ComposeScene(showComposer: self.$showComposer)
+            })
         }
+    }
+}
+
+fileprivate struct ModalButton: View {
+    @Binding var show: Bool //값을 저장하지 않고 속성을 바꾸기 위한 속성
+    
+    var body: some View {
+        Button(action: {
+            self.show = true
+        }, label: {
+            Image(systemName: "plus")
+        })
     }
 }
 
